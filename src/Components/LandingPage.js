c38e25d71932f1c3e864ab9307da6d1527c573df
ampaire <femayapr@gmail.com>
2020-07-02 21:38:00 -0600

    Add the landing page

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import MdPaperPlane from 'react-ionicons/lib/MdPaperPlane';
+import IosLogIn from 'react-ionicons/lib/IosLogIn';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="btns">
+          <div className="btn-outlined">
+            <div>
+              <button
+                className="btn btn-outline-primary"
+                type="button"
+              >
+                <IosLogIn color="#ffff" className="icons" />
+                SignUp to get started
+              </button>
+              <button
+                className="btn btn-outline-primary"
+                type="button"
+              >
+                <IosLogIn color="#ffff" className="icons" />
+                Login
+              </button>
+            </div>
+            <div>
+              <button
+                className="btn btn-outline-primary"
+                type="button"
+              >
+                <MdPaperPlane color="#ffff" className="icons" />
+                Explore
+              </button>
+            </div>
+          </div>
+          <div>
+            <h2 className="app-title">The CookBook</h2>
+            <blockquote>
+              <p className="app-index-quote">
+                Explore the world's Best dishes
+              </p>
+              <footer className="author">― Effie Phemia Ampaire</footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
