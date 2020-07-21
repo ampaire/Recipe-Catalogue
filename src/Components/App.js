502e0f7d175b2e1aae6b8869dda7fa3233c161ba
ampaire <femayapr@gmail.com>
2020-07-21 17:13:00 -0600

    Fix TSE suggestions

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 import AllMeals from '../Containers/AllRecipes';
 import MealDetails from '../Containers/ItemDetails';
-import Main from './LandingPage';
 import NavBar from '../Containers/Nav';
 import AllCategories from '../Containers/Explore';
 
@@ -11,8 +10,7 @@ const App = () => (
   <div>
     <NavBar />
     <Switch>
-      <Route path="/" component={Main} exact />
-      <Route path="/explore" component={AllCategories} exact />
+      <Route path="/" component={AllCategories} exact />
       <Route path="/category/:category" component={AllMeals} />
       <Route path="/meal/:id" component={MealDetails} />
     </Switch>
