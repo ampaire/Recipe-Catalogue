a4eeae1f72c7461cad1c106608d41bc4f17a6bd8
ampaire <femayapr@gmail.com>
2020-07-28 14:15:49 -0600

    Add changes

--- src/Containers/Nav.js
+++ src/Containers/Nav.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <div className="nav header">
     <Link className="home-link" to="/">
-      <h4 className=" Nav-text"> The CookBook </h4>
+      <h4 className=" Nav-text"> The CookBook 😋</h4>
     </Link>
   </div>
 );
